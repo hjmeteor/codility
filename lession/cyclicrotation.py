852cd285b37a78bf9016dcd174a00ac57b28a3c0
Meteor <93meteor@gmail.com>
2016-11-10 20:51:42 +0000

    lesson 2 cyclicrotation

---
+++ lession/cyclicrotation.py
@@ -0,0 +1,16 @@
+def solution(A, K):
+    # write your code in Python 2.7
+    if A != []:
+        temp = 0
+        for k in range(K):
+            temp = A[-1]
+            for n in range(len(A) - 2, -1, -1):
+                A[n + 1] = A[n]
+            A[0] = temp
+        return A
+    else:
+        return A
+    pass
+
+
+print(solution([3, 8, 9, 7, 6], 3))
