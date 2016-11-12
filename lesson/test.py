dd65eccced2cea699f99d9b2bfc412f496a542b9
Meteor <93meteor@gmail.com>
2016-11-12 02:24:30 +0000

    lesson pdf

--- lesson/test.py
+++ lesson/test.py
@@ -1,2 +1,15 @@
-bn = bin(1)
-print(bn)
+def bin_menghan(x):
+    index = 0
+    y = 0
+    while x != 1 and x != -1:
+        y += (10**index) * (x % 2)
+        x = x / 2
+        index += 1
+    y += 10**index
+    if x == -1:
+        y = -y
+    return y
+
+
+print(bin_menghan(-5))
+print(-5 % 2)
